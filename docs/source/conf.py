39f54f60b04545a8049cf4ab3a62179da34414bf
schristophe <steven.christophe@obspm.fr>
2021-02-17 12:20:31 +0100

    updating version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2021, Steven Christophe'
 author = 'Steven Christophe'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
