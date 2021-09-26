3d4af59a0336177bf98486b39e40398a4463fbb4
schristophe <steven.christophe.pro@gmail.com>
2021-09-26 23:45:04 +0200

    default source_suffix is sufficient

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'morse'
-copyright = '2020-2021, Steven Christophe'
-author = 'Steven Christophe'
+project = "morse"
+copyright = "2020-2021, Steven Christophe"
+author = "Steven Christophe"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,11 +31,15 @@ release = '0.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','nbsphinx',
-        'sphinx.ext.mathjax']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -43,8 +48,7 @@ exclude_patterns = []
 
 # The file extensions of source files. Sphinx considers the files with this
 # suffix as sources.
-source_suffix = ['.rst', '.ipynb']
-
+# source_suffix = {".rst": "restructuredtext"}
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,11 +56,11 @@ source_suffix = ['.rst', '.ipynb']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # -- Options for autodoc -----------------------------------------------------
