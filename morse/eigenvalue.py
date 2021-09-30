cf22fdfacdde6c4cde20089057322ceb6707a47c
schristophe <steven.christophe.pro@gmail.com>
2021-09-30 20:04:49 +0200

    replace tabulated eigenvalues for Rossby modes
    Tabulated data that were used until now had very few points near
    -k(-k+1) while this is a region where the eigenvalue varies a lot.
    This could cause a problem when computing the periods of modes
    of low spin parameter or stretching a spectrum that contains such
    modes. New tabulated data prevent this problem by adding a lot more
    points near this limit.

--- morse/eigenvalue.py
+++ morse/eigenvalue.py
@@ -44,7 +44,7 @@ class Eigenvalue(object):
             else:
                 self.lamb = data[:, l - abs(self.m) + 1]
         elif self.k + abs(self.m) <= 0 and self.m != 0:
-            file_rel_path = f"lambda/lambda_m{self.m}_k{self.k}.txt"
+            file_rel_path = f"lambda/lambda_m{self.m}_k{self.k}_gyre.txt"
             if pkg_resources.resource_exists("morse", file_rel_path):
                 file_abs_path = pkg_resources.resource_filename("morse", file_rel_path)
                 data = np.genfromtxt(file_abs_path)
@@ -53,7 +53,7 @@ class Eigenvalue(object):
                     f"""Tabulated values for lambda(m={self.m},k={self.k})
                         are not available yet."""
                 )
-            self.eta = data[1:, 0]
-            self.lamb = data[1:, 1]  # at s=-k(-k+1), lambda is equal to 0
+            self.eta = data[:, 0]
+            self.lamb = data[:, 1]
         else:
             sys.exit("Radial modes are not handled.")
